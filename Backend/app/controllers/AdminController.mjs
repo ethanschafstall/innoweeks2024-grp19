c19a6585a96413ab1b90b43fc1c50dd8145c9089
ethanschafstall <ethans9999@gmail.com>
2024-06-03 18:36:44 +0200

    updated controller reponse text
    changed from french to english to standardize codebase

--- Backend/app/controllers/AdminController.mjs
+++ Backend/app/controllers/AdminController.mjs
@@ -4,35 +4,35 @@ import { privateKey } from "../privateKey.mjs";
 
 const router = express.Router();
 
-export const getAllUsers = async ( req, res) => {
+export const getAllUsers = async (req, res) => {
     const authorizationHeader = req.headers.authorization;
 
     // Checking if authorization header exists
     if (!authorizationHeader) {
         // If authorization header is missing, return 401 Unauthorized status
-        const message = `Vous n'avez pas fourni de jeton d'authentification. Ajoutez-en un dans l'en-tête de la requête.`;
+        const message = `You did not provide an authentication token. Please add one to the request header.`;
         return res.status(401).json({ message });
     }
 
-    const token = authorizationHeader.split(" ")[2]; 
+    const token = authorizationHeader.split(" ")[2];
 
     jwt.verify(token, privateKey, async (error, decodedToken) => {
         if (error) {
             console.log(error);
             let message;
             if (error.name === 'TokenExpiredError') {
-                message = `Votre session a expiré. Veuillez vous reconnecter.`;
+                message = `Your session has expired. Please log in again.`;
             } else if (error.name === 'JsonWebTokenError') {
-                message = `La demande est invalide. Veuillez vérifier vos informations de connexion.`;
+                message = `The request is invalid. Please check your login details.`;
             } else {
-                message = `L'utilisateur n'est pas autorisé à accéder à cette ressource.`;
+                message = `The user is not authorized to access this resource.`;
             }
             return res.status(401).json({ message });
         }
         const userRole = decodedToken.userRole;
 
         if (userRole !== "admin") {
-            const message = `L'utilisateur n'est pas autorisé à accéder à cette ressource`;
+            const message = `The user is not authorized to access this resource.`;
             return res.status(401).json({ message });
         }
 
@@ -49,49 +49,49 @@ export const getAllUsers = async ( req, res) => {
     });
 };
 
-export const getUsers = async ( req, res) => {
+export const getUsers = async (req, res) => {
     const authorizationHeader = req.headers.authorization;
 
     // Checking if authorization header exists
     if (!authorizationHeader) {
         // If authorization header is missing, return 401 Unauthorized status
-        const message = `Vous n'avez pas fourni de jeton d'authentification. Ajoutez-en un dans l'en-tête de la requête.`;
+        const message = `You did not provide an authentication token. Please add one to the request header.`;
         return res.status(401).json({ message });
     }
 
     let query = "";
 
-    // checks is the reqest body contains a username
+    // Checks if the request body contains a username
     if (req.body.username) {
         query = req.body.username;
     } 
-    else if (req.params.username) // checks is the reqest params contains a username
+    else if (req.params.username) // Checks if the request params contain a username
     {
         query = req.params.username;
     } else {
-        const message = "Aucun nom d'utilisateur fourni.";
+        const message = "No username provided.";
         return res.status(400).json({ message });
     }
 
-    const token = authorizationHeader.split(" ")[2]; 
+    const token = authorizationHeader.split(" ")[2];
 
     jwt.verify(token, privateKey, async (error, decodedToken) => {
         if (error) {
             console.log(error);
             let message;
             if (error.name === 'TokenExpiredError') {
-                message = `Votre session a expiré. Veuillez vous reconnecter.`;
+                message = `Your session has expired. Please log in again.`;
             } else if (error.name === 'JsonWebTokenError') {
-                message = `La demande est invalide. Veuillez vérifier vos informations de connexion.`;
+                message = `The request is invalid. Please check your login details.`;
             } else {
-                message = `L'utilisateur n'est pas autorisé à accéder à cette ressource.`;
+                message = `The user is not authorized to access this resource.`;
             }
             return res.status(401).json({ message });
         }
         const userRole = decodedToken.userRole;
 
         if (userRole !== "admin") {
-            const message = `L'utilisateur n'est pas autorisé à accéder à cette ressource`;
+            const message = `The user is not authorized to access this resource.`;
             return res.status(401).json({ message });
         }
 
@@ -102,11 +102,12 @@ export const getUsers = async ( req, res) => {
 
             return res.status(200).json({ users: rows });
         } catch (error) {
-            const message = "Internal Server Error"
+            const message = "Internal Server Error";
             return res.status(500).json({ message });
         }
     });
 };
+
 
 router.get('/', getAllUsers);
 
--- Backend/app/controllers/UserController.mjs
+++ Backend/app/controllers/UserController.mjs
@@ -13,7 +13,7 @@ export const get = (req, res) => {
     // Checking if authorization header exists
     if (!authorizationHeader) {
         // If authorization header is missing, return 401 Unauthorized status
-        const message = `Vous n'avez pas fourni de jeton d'authentification. Ajoutez-en un dans l'en-tête de la requête.`;
+        const message = `You did not provide an authentication token. Please add one to the request header.`;
         return res.status(401).json({ message });
     } else {
         // Extracting token from authorization header
@@ -21,10 +21,10 @@ export const get = (req, res) => {
         
         // Verifying the token with the secret key
         jwt.verify(token, privateKey, (error, decodedToken) => {
-            console.log(token)
+            console.log(token);
             if (error) {
                 // If token verification fails, return 401 Unauthorized status
-                const message = `L'utilisateur n'est pas autorisé à accéder à cette ressource`;
+                const message = `The user is not authorized to access this resource.`;
                 return res.status(401).json({ message });
             }
             // Extracting user ID from the decoded token
@@ -32,7 +32,7 @@ export const get = (req, res) => {
             // Checking if the user ID in the request body matches the one in the token
             if (req.params.username && req.params.username !== userId) {
                 // If user ID in the request body doesn't match the one in the token, return 401 Unauthorized status
-                const message = `L'utilisateur n'est pas autorisé à accéder à cette ressource`;
+                const message = `The user is not authorized to access this resource.`;
                 return res.status(401).json({ message });
             } else {
                 // If everything is fine, proceed to the next middleware
@@ -42,6 +42,7 @@ export const get = (req, res) => {
     }
 
 };
+
 
 
 router.get('/:username', get);
