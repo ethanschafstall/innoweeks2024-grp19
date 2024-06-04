2f872cf3221e9986ffcf920aeb9d41a7b2d779ee
ethanschafstall <ethans9999@gmail.com>
2024-06-04 14:39:18 +0200

    updated feeling router
    commented unimplemented controllers

--- Backend/app/routes/Feelings.mjs
+++ Backend/app/routes/Feelings.mjs
@@ -1,6 +1,7 @@
 import express from "express";
 // import { } from "../controllers/FeelingsController.mjs";
 import { connectToDatabase } from "../tools/sqlConnection.mjs";
+import { setFeeling } from "../controllers/FeelingsController.mjs";
 
 const connectToDatabaseMiddleware = async (req, res, next) => {
     try {
@@ -13,7 +14,13 @@ const connectToDatabaseMiddleware = async (req, res, next) => {
 };
 const router = express.Router();
 
-router.get ('/', connectToDatabaseMiddleware, getAllUsers);
-router.get ('/:username', connectToDatabaseMiddleware, getUsers);
+// Route for getting a single feeling from a user (other than self) for notification
+// router.get ('/:id', connectToDatabaseMiddleware, getFeeling);
+
+// Route for geting all the feeling from user
+// router.get ('/:username', connectToDatabaseMiddleware, getFeelings);
+
+// Route for creating a new feeling
+router.post('/', connectToDatabaseMiddleware, setFeeling);
 
 export default router;
