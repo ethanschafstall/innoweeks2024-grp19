4dc5329c1c81f3bced0051064f7caa093f797e0a
ethanschafstall <ethans9999@gmail.com>
2024-06-04 23:31:04 +0200

    updated server
    added imports for notification handling + some tools

--- Backend/app/server.mjs
+++ Backend/app/server.mjs
@@ -3,27 +3,47 @@ import https from 'node:https';
 import http from 'node:http';
 import fs from 'node:fs';
 import express from 'express';
+
+// Import real-time communication libraries for notifications
+import mqtt from 'mqtt';
+import { Server as SocketIOServer } from 'socket.io';
+import { HubConnectionBuilder } from '@microsoft/signalr';
+
+// Import routes 
 import userRoute from './routes/User.mjs';
-import { loginRoute } from './routes/login.mjs';
-import { testRoute } from './routes/Test.mjs';
-import { registerRoute } from './routes/Register.mjs';
+import registerRoute from './routes/Register.mjs';
 import feelingsRoute from './routes/Feelings.mjs';
+import loginRoute from './routes/Login.mjs';
 import adminRoute from './routes/Admin.mjs';
+import { testRoute } from './routes/Test.mjs';
+
+// Middleware
+import { databaseConnectionMiddleware } from "./tools/connectToDatabaseMiddleware.mjs";
 import cors from 'cors'
+
+// Tools
 import cookieParser from 'cookie-parser';
+import bodyParser from 'body-parser';
 
 // Read SSL certificate and key files
 const options = {
   key: fs.readFileSync('openssl/privateKey.key'),
-  cert: fs.readFileSync('openssl/certificate.crt'), // Make sure this path and file extension are correct
+  cert: fs.readFileSync('openssl/certificate.crt'),
 };
 
-// Initialize the Express application
+// Initialize app
 const app = express();
+const server = https.createServer(options, app)
+
+const mqttClient = mqtt.connect('mqtt://broker.hivemq.com');
+const io = new SocketIOServer(server);
+
+// Apply middleware & tools to app
+app.use(express.json());
+app.use(databaseConnectionMiddleware);
 app.use(cors());
 app.use(cookieParser());
-// Middleware for parsing JSON responses
-app.use(express.json());
+app.use(bodyParser.json());
 
 // Define routes
 app.use('/users', adminRoute);
@@ -33,12 +53,21 @@ app.use('/register', registerRoute);
 app.use('/test', testRoute);
 app.use('/feeling', feelingsRoute);
 
+// socket.io connection handling
+io.on('connection', (socket) => {
+  console.log('a user connected');
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
+});
+
+
 // Define ports for HTTP and HTTPS
-const portHttp = 443;  // Standard HTTP port
-const portHttps = 4433;  // Standard HTTPS port
+const portHttp = 443;
+const portHttps = 4433;
 
 // Start the HTTPS server
-https.createServer(options, app).listen(portHttps, () => {
+server.listen(portHttps, () => {
   console.log(`Server HTTPS running on https://localhost:${portHttps}`);
 });
 
