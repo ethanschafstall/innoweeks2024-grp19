20821058bd8bf2b60a8d7b3846b34af78570ed58
ethanschafstall <ethans9999@gmail.com>
2024-06-04 07:13:06 +0200

    updated server
    updated server to handle cross-origin resource sharing, cookie parsing

--- Backend/app/server.mjs
+++ Backend/app/server.mjs
@@ -1,49 +1,44 @@
-
-
-
-// Librairies et ressources
-
+// Import necessary libraries
 import https from 'node:https';
 import http from 'node:http';
 import fs from 'node:fs';
+import express from 'express';
+import userRoute from './routes/User.mjs';
+import { loginRoute } from './routes/login.mjs';
+import { registerRoute } from './routes/Register.mjs';
+import adminRoute from './routes/Admin.mjs';
+import cors from 'cors'
+import cookieParser from 'cookie-parser';
 
-import express from "express";
-import userRoute from "./routes/User.mjs";
-import { loginRoute } from "./routes/login.mjs";
-import { registerRoute } from "./routes/Register.mjs";
-import adminRoute from "./routes/Admin.mjs";
-
-
+// Read SSL certificate and key files
 const options = {
   key: fs.readFileSync('openssl/privateKey.key'),
-  cert: fs.readFileSync('openssl/certificate.key'),
+  cert: fs.readFileSync('openssl/certificate.crt'), // Make sure this path and file extension are correct
 };
 
-
+// Initialize the Express application
 const app = express();
-
-// Middleware pour la lecture des réponses formatées en json
+app.use(cors());
+app.use(cookieParser());
+// Middleware for parsing JSON responses
 app.use(express.json());
 
-// Les routes
+// Define routes
 app.use('/users', adminRoute);
-
 app.use('/user', userRoute);
 app.use('/login', loginRoute);
 app.use('/register', registerRoute);
 
-let portHttp = 443;
-let portHttps = 4433;
-// Démarrage du serveur
-// app.listen(port, () => {
-//     console.log(`Server running at http://localhost:${port}`);
-// });
+// Define ports for HTTP and HTTPS
+const portHttp = 443;  // Standard HTTP port
+const portHttps = 4433;  // Standard HTTPS port
 
-
- https.createServer(options, app).listen(1111, () => {
-    console.log(`Server HTTPS running on https://localhost:${portHttps}`)
+// Start the HTTPS server
+https.createServer(options, app).listen(portHttps, () => {
+  console.log(`Server HTTPS running on https://localhost:${portHttps}`);
 });
- 
+
+// Start the HTTP server
 http.createServer(app).listen(portHttp, () => {
-    console.log(`Server HTTP running on http://localhost:${portHttp}`)
+  console.log(`Server HTTP running on http://localhost:${portHttp}`);
 });
