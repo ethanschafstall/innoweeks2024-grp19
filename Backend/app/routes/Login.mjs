afebbf92cbd7020f0ac1346216a458c74dc0cbc4
ethanschafstall <ethans9999@gmail.com>
2024-06-04 07:15:28 +0200

    updated login route
    sets cookie

--- Backend/app/routes/Login.mjs
+++ Backend/app/routes/Login.mjs
@@ -2,6 +2,7 @@ import express from "express"; // Importing express for router creation
 import { connectToDatabase } from "../tools/sqlConnection.mjs";
 import { checkHash } from "../tools/hash.mjs";
 import { generateToken } from "../tools/createToken.mjs";
+import cookie from 'cookie'
 
 const loginRoute = express(); // Creating a new instance of express router
 
@@ -17,27 +18,41 @@ const connectToDatabaseMiddleware = async (req, res, next) => {
 
 // Endpoint for handling user login
 loginRoute.post("/", connectToDatabaseMiddleware, async (req, res) => {
-    const { username, password } = req.body;
-    const queryString = `SELECT * FROM t_users WHERE useUsername = ?`;
+  const { username, password } = req.body;
+  const queryString = `SELECT * FROM t_users WHERE useUsername = ?`;
 
-    try {
-        
-        const [result] = await req.dbConnection.execute(queryString, [username]);
-        const user = result[0];
-        const salt = user.useSalt;
-        const stockedHash = user.usePassword;
+  try {
+      const [result] = await req.dbConnection.execute(queryString, [username]);
+      const user = result[0];
 
-        if (stockedHash != checkHash(salt, password)) {
-            const message = `Invalid username or password.`;
-            return res.status(401).json({ message });
-        }
-        const message = `User has successfully logged in.`;
-        const token = generateToken(user);
-        return res.status(200).json({ message, token });
-    } catch (error) {
+      if (!user) {
+          return res.status(401).json({ message: "Invalid username or password." });
+      }
+
+      const salt = user.useSalt;
+      const storedHash = user.usePassword;
+
+      if (storedHash !== checkHash(salt, password)) {
+          return res.status(401).json({ message: "Invalid username or password." });
+      }
+
+      const message = `User has successfully logged in.`;
+      const token = generateToken(user);
+
+      res.cookie('authToken', token, {
+          httpOnly: true, // Accessible only by web server
+          secure: true, // Ensures the cookie is only sent over HTTPS
+          sameSite: 'strict', // Ensures the cookie is sent only to the same site
+          maxAge: 24 * 60 * 60 * 1000, // Cookie expires in 24 hours
+          path: '/'
+      });
+    
+      return res.status(200).json({ message, token});
+  } catch (error) {
       console.error("Error logging in:", error);
-      res.status(500).json({ error: "Internal Server Error" });
-    }
+      return res.status(500).json({ error: "Internal Server Error" });
+  }
 });
+
 
 export { loginRoute }; // Exporting the router for use in other files
