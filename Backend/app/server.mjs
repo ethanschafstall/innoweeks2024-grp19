7a1cd825a11fbcdcdc1756ef1390322bc18ecf90
ethan <ethans9999@gmail.com>
2024-06-12 08:22:43 +0200

    updated server
    removed unused imports and initializations

--- Backend/app/server.mjs
+++ Backend/app/server.mjs
@@ -1,13 +1,6 @@
 // Import necessary libraries
-import https from 'node:https';
 import http from 'node:http';
-import fs from 'node:fs';
 import express from 'express';
-
-// Import real-time communication libraries for notifications
-import mqtt from 'mqtt';
-import { Server as SocketIOServer } from 'socket.io';
-import { HubConnectionBuilder } from '@microsoft/signalr';
 
 // Import routes 
 import userRoute from './routes/User.mjs';
@@ -28,18 +21,8 @@ import cors from 'cors'
 import cookieParser from 'cookie-parser';
 import bodyParser from 'body-parser';
 
-// Read SSL certificate and key files
-const options = {
-  key: fs.readFileSync('openssl/privateKey.key'),
-  cert: fs.readFileSync('openssl/certificate.crt'),
-};
-
 // Initialize app
 const app = express();
-const server = https.createServer(options, app)
-
-const mqttClient = mqtt.connect('mqtt://broker.hivemq.com');
-const io = new SocketIOServer(server);
 
 // Apply middleware & tools to app
 app.use(express.json());
@@ -58,24 +41,8 @@ app.use('/feeling', feelingsRoute);
 app.use('/friendsGroup', friendsGroupRoute)
 app.use('/friend', friendsRoute)
 
-
-// socket.io connection handling
-io.on('connection', (socket) => {
-  console.log('a user connected');
-  socket.on('disconnect', () => {
-    console.log('user disconnected');
-  });
-});
-
-
 // Define ports for HTTP and HTTPS
-const portHttp = 443;
-const portHttps = 4433;
-
-// Start the HTTPS server
-server.listen(portHttps, () => {
-  console.log(`Server HTTPS running on https://localhost:${portHttps}`);
-});
+const portHttp = 8080;
 
 // Start the HTTP server
 http.createServer(app).listen(portHttp, () => {
