01de38b710e03d85996c225a5492b899f06277f1
ethan <ethans9999@gmail.com>
2024-06-14 09:52:41 +0200

    changed route names

--- Backend/app/controllers/FriendsController.mjs
+++ Backend/app/controllers/FriendsController.mjs
@@ -1,52 +1,50 @@
 import jwt from 'jsonwebtoken';
 import { privateKey } from "../privateKey.mjs";
-import { formatDateToSQL } from "../tools/dateFormater.mjs";
-import { notifier } from '../services/notifications/notificationManager.mjs';
-import { postFriendsGroup } from './FriendsGroupController.mjs';
+
 export const postFriends = async (req, res) => {
     const token = req.cookies.authToken;
-    const body = req.body;
+    const { groId, useUsername } = req.body;
 
     if (!token) {
-        const message = `You did not provide an authentication token.`;
-        return res.status(401).json({ message });
+        return res.status(401).json({ message: "You did not provide an authentication token." });
     }
 
-    if (!body || !body.groName || !body.useUsername) {
-        const message = `You did not provide a valid request body, group name, or user username.`;
-        return res.status(400).json({ message });
+    if (!groId || !useUsername) {
+        return res.status(400).json({ message: "You did not provide a valid request body, group name, or user username." });
     }
 
     jwt.verify(token, privateKey, async (error, decodedToken) => {
         if (error) {
-            console.log(error);
-            let message;
-            if (error.name === 'TokenExpiredError') {
-                message = `Your session has expired. Please log in again.`;
-            } else if (error.name === 'JsonWebTokenError') {
-                message = `The request is invalid. Please check your login details.`;
-            } else {
-                message = `The user is not authorized to access this resource.`;
-            }
+            console.error("JWT Verification Error:", error);
+            const message = error.name === 'TokenExpiredError'
+                ? "Your session has expired. Please log in again."
+                : "The request is invalid. Please check your login details.";
             return res.status(401).json({ message });
         }
 
         try {
-            const groName = body.groName;
-            const useUsername = body.useUsername;
+            const userInGroupQuery = `
+                SELECT * FROM t_group_members
+                WHERE fkGroup = ? AND fkUser = (SELECT userId FROM t_users WHERE useUsername = ?)
+            `;
+            const [userInGroupRows] = await req.dbConnection.execute(userInGroupQuery, [groId, useUsername]);
 
-            // Obtener o crear el ID del grupo
-            const fkGroup = await getOrCreateGroupId(req.dbConnection, groName, token);
+            if (userInGroupRows.length > 0) {
+                return res.status(400).json({ message: `The user ${useUsername} is already assigned to the group ${groId}.` });
+            }
 
-            // Obtener el ID del usuario
-            const fkUser = await getUserId(req.dbConnection, useUsername);
+            const userQuery = `SELECT userId FROM t_users WHERE useUsername = ?`;
+            const [userRows] = await req.dbConnection.execute(userQuery, [useUsername]);
 
-            // Insertar el amigo en t_groups_members
+            if (userRows.length === 0) {
+                return res.status(400).json({ message: `The user ${useUsername} does not exist.` });
+            }
+
+            const fkUser = userRows[0].userId;
             const insertQuery = `INSERT INTO t_group_members (fkGroup, fkUser) VALUES (?, ?)`;
-            await req.dbConnection.execute(insertQuery, [fkGroup, fkUser]);
+            await req.dbConnection.execute(insertQuery, [groId, fkUser]);
 
-            // Respuesta exitosa
-            return res.status(200).json({ message: `Friend added successfully to the group ${groName}.` });
+            return res.status(200).json({ message: `Friend added successfully to the group ${groId}.` });
         } catch (error) {
             console.error("Error adding friend to group:", error);
             return res.status(500).json({ error: "Internal Server Error" });
@@ -54,36 +52,65 @@ export const postFriends = async (req, res) => {
     });
 };
 
-// Función para obtener o crear el ID del grupo
-const getOrCreateGroupId = async (dbConnection, groName, authToken) => {
-    const groupIdQuery = `SELECT groupId FROM t_groups WHERE groName = ?`;
-    let [groupIdRows] = await dbConnection.execute(groupIdQuery, [groName]);
-    
-    if (!groupIdRows || groupIdRows.length === 0) {
-        // Si el grupo no existe, crear uno nuevo usando postFriendsGroup
-        const newGroupRequest = {
-            body: { groName },
-            cookies: { authToken }, // Añadir la cookie authToken
-            dbConnection,
-        };
-        const newGroup = await postFriendsGroup(newGroupRequest);
-        if (!newGroup || !newGroup.groupId) {
-            throw new Error(`Failed to create the group ${groName}.`);
-        }
-        groupIdRows = [{ groupId: newGroup.groupId }];
-    }
-    
-    return groupIdRows[0].groupId;
-};
+export const getFriendsFeelings = async (req, res) => {
+    const token = req.cookies.authToken;
 
-// Función para obtener el ID del usuario
-const getUserId = async (dbConnection, useUsername) => {
-    const userIdQuery = `SELECT userId FROM t_users WHERE useUsername = ?`;
-    const [userIdRows] = await dbConnection.execute(userIdQuery, [useUsername]);
-    
-    if (!userIdRows || userIdRows.length === 0) {
-        throw new Error(`The user with the username ${useUsername} does not exist.`);
+    if (!token) {
+        return res.status(401).json({ message: "You did not provide an authentication token." });
     }
-    
-    return userIdRows[0].userId;
+
+    jwt.verify(token, privateKey, async (error, decodedToken) => {
+        if (error) {
+            console.error("JWT Verification Error:", error);
+            const message = error.name === 'TokenExpiredError'
+                ? "Your session has expired. Please log in again."
+                : "The request is invalid. Please check your login details.";
+            return res.status(401).json({ message });
+        }
+
+        const userId = decodedToken.id;
+
+        try {
+            // First query to get the group owners where the authenticated user is a member
+            const groupOwnersQuery = `
+                SELECT g.fkUser
+                FROM t_group_members as gm
+                JOIN t_groups as g ON gm.fkGroup = g.groupId
+                WHERE gm.fkUser = ?
+            `;
+            const [groupOwnersRows] = await req.dbConnection.execute(groupOwnersQuery, [userId]);
+
+            const groupOwnerIds = groupOwnersRows.map(row => row.fkUser);
+
+
+            // Initialize an array to hold the feelings
+            const feelings = [];
+
+            // Iterate over each group owner and get their last feeling
+            for (const ownerId of groupOwnerIds) {
+                const feelingsQuery = `
+                    SELECT f.feeMood, u.useUsername
+                    FROM t_feelings as f
+                    JOIN t_users as u ON f.fkUser = u.userId
+                    WHERE f.fkUser = ?
+                    ORDER BY f.feelingsId DESC
+                    LIMIT 1
+                `;
+                const [feelingsRows] = await req.dbConnection.execute(feelingsQuery, [ownerId]);
+
+                if (feelingsRows.length > 0) {
+                    feelings.push(feelingsRows[0]);
+                }
+            }
+
+            if (feelings.length > 0) {
+                return res.status(200).json({ feelings });
+            } else {
+                return res.status(404).json({ message: "No feelings found for the specified users." });
+            }
+        } catch (error) {
+            console.error("Error fetching feelings:", error);
+            return res.status(500).json({ error: "Internal Server Error" });
+        }
+    });
 };
