7bdcc47f02e9b7d3335e12e8b37e2706c3f212f7
ethan <ethans9999@gmail.com>
2024-06-14 11:28:32 +0200

    updated hast tool
    converted all to using the crypto library

--- Backend/app/tools/hash.mjs
+++ Backend/app/tools/hash.mjs
@@ -1,13 +1,13 @@
-import CryptoJS from "crypto-js";
 import dotenv from 'dotenv';
 import crypto from 'crypto'
 
 dotenv.config();
-
 const pepper = process.env.PEPPER;
 
-export const random= () => { crypto.randomBytes(256).toString('base64')}
+export const random = () => { return crypto.randomBytes(64).toString('base64')}
 
-export const authentication = (password, salt) => { 
-  return CryptoJS.SHA256(salt + password + pepper).toString(CryptoJS.enc.Hex)
-}
+export const authentication = (password, salt, pepper) => {
+  const hash = crypto.createHash('sha256');
+  hash.update(salt + password + pepper);
+  return hash.digest('hex');
+};
