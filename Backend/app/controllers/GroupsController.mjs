a0ec788c555f27e4d12eee521bbe95852c96de06
ethan <ethans9999@gmail.com>
2024-06-14 14:25:40 +0200

    added getGroups request

--- Backend/app/controllers/GroupsController.mjs
+++ Backend/app/controllers/GroupsController.mjs
@@ -42,3 +42,36 @@ export const postFriendsGroup = async (req, res) => {
         }
     });
 };
+
+export const getGroups = async (req, res) => {
+    const token = req.cookies.authToken;
+
+    if (!token) {
+        return res.status(401).json({ message: "You did not provide an authentication token." });
+    }
+
+    jwt.verify(token, privateKey, async (error, decodedToken) => {
+        if (error) {
+            console.error("JWT Verification Error:", error);
+            const message = error.name === 'TokenExpiredError'
+                ? "Your session has expired. Please log in again."
+                : "The request is invalid. Please check your login details.";
+            return res.status(401).json({ message });
+        }
+
+        try {
+            const getGroupsQuery = `SELECT * FROM t_groups WHERE fkUser = ?`;
+            const [groups] = await req.dbConnection.execute(getGroupsQuery, [decodedToken.id]);
+            console.log(groups)
+            if (groups.length < 0) {
+                return res.status(400).json({ message: `The user hasn't created any groups` });
+            }
+
+            console.table(groups)
+            return res.status(200).json({ groups: groups });
+        } catch (error) {
+            console.error("Error creating group:", error);
+            return res.status(500).json({ error: "Internal Server Error" });
+        }
+    });
+};
