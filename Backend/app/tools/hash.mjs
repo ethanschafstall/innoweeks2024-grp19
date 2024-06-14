0ca294f97b78dc809fd4623d9ded874b7aeb0552
ethan <ethans9999@gmail.com>
2024-06-14 11:16:28 +0200

    updated hash tool
    simplified the tools (DRY)

--- Backend/app/tools/hash.mjs
+++ Backend/app/tools/hash.mjs
@@ -1,37 +1,13 @@
 import CryptoJS from "crypto-js";
 import dotenv from 'dotenv';
+import crypto from 'crypto'
 
 dotenv.config();
 
 const pepper = process.env.PEPPER;
 
+export const random= () => { crypto.randomBytes(256).toString('base64')}
 
-export function generateHash(message) {
-  const salt = addSalt(message.length);
-  const hash = CryptoJS.SHA256(salt + message + pepper);
-  return [hash.toString(CryptoJS.enc.Hex), salt];
+export const authentication = (password, salt) => { 
+  return CryptoJS.SHA256(salt + password + pepper).toString(CryptoJS.enc.Hex)
 }
-
-export function checkHash(salt, message) {
-  const hash = CryptoJS.SHA256(salt + message + pepper);
-  return hash.toString(CryptoJS.enc.Hex);
-}
-
-function addSalt(passwordLength) {
-  const desiredSaltLength = 15;
-  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-  const charactersLength = characters.length;
-  
-  let salt = '';
-  for (let i = 0; i < passwordLength; i++) {
-      salt += characters.charAt(Math.floor(Math.random() * charactersLength));
-  }
-  
-  while (salt.length < desiredSaltLength) {
-      salt += salt;
-  }
-  salt = salt.slice(0, desiredSaltLength);
-  
-  return salt;
-}
-
